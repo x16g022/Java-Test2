29e9a94be53c4991024e0dd255469110bbb58c70
WIN_NT\x16g022 <x16g022@chiba-fjb.ac.jp>
2017-12-01 10:01:08 +0900

    コミット（コミット2）

--- src/popokun.java
+++ src/popokun.java
@@ -2,8 +2,9 @@
 public class popokun {
 
 	public static void main(String[] args) {
-		System.out.println("    _、_");
-		System.out.println("(   ,_ﾉ` ）");
+		System.out.println("       _、_");
+		System.out.println("   (   ,_ﾉ` ）");
+		System.out.println("【W e l c o m e】 ");
 
 	}
 

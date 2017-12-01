840be9f17b61a3444bb7c6f1a4c98634a3b1a70b
WIN_NT\x16g022 <x16g022@chiba-fjb.ac.jp>
2017-12-01 10:41:40 +0900

    コミット3

--- src/popokun.java
+++ src/popokun.java
@@ -5,6 +5,7 @@ public class popokun {
 		System.out.println("    _、_");
 		System.out.println("(   ,_ﾉ` ）");
 		System.out.println("【U n w e l c o m e】");
+		System.out.println("【警 告 ( w a r n i n g) 】");
 
 	}
 
